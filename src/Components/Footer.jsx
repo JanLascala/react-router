25a957490ccaab8b925ddd8426ac128388d6722a
JanLascala <lascala.jan@gmail.com>
2025-04-01 17:40:12 +0200

    feat: enhance Footer and Posts components with improved layout and styling

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,14 +1,44 @@
 export default function Footer() {
     return (
-        <>
-            <div
-                className="row-cols-4 justify-content-center align-items-center g-2"
-            >
-                <div className="col">Column</div>
-                <div className="col">Column</div>
-                <div className="col">Column</div>
-            </div>
+        <footer className="bg-dark text-light py-4">
+            <div className="container d-flex justify-content-between">
+                <div className="row">
+                    <div className="col">
+                        <ul>
+                            <li>Terms of use</li>
+                            <li>privacy policy news</li>
+                            <li>Add choices</li>
+                            <li>Advertising</li>
+                            <li>Jobs</li>
+                            <li>Subssciprions</li>
+                            <li>Talent Workshop</li>
+                            <li>Cpsc Certificate</li>
+                            <li>Ratings</li>
+                            <li>Shop here</li>
+                            <li>Contact Us</li>
 
-        </>
-    )
+                        </ul>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <ul>
+                            <li>Terms of use</li>
+                            <li>privacy policy news</li>
+                            <li>Add choices</li>
+                            <li>Advertising</li>
+                            <li>Jobs</li>
+                            <li>Subssciprions</li>
+                            <li>Talent Workshop</li>
+                            <li>Cpsc Certificate</li>
+                            <li>Ratings</li>
+                            <li>Shop here</li>
+                            <li>Contact Us</li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
 }
--- src/Pages/Posts.jsx
+++ src/Pages/Posts.jsx
@@ -1,28 +1,29 @@
 import mtvCartoons from "../Data/Characters";
+
 export default function Posts() {
-
-
     console.log(mtvCartoons);
+
     const CartoonCard = ({ cartoon }) => {
         return (
+            <div className="card bg-secondary text-light m-3" >
+                <div className="card-body text-center">
+                    <img
+                        className="card-img-top mb-3"
+                        src={cartoon.image}
 
-            <div className="card bg-secondary m-3 justify-content-center">
-                <div><img className="photo just" src={cartoon.image} alt={cartoon.character} /></div>
-                <div><h3>{cartoon.character}</h3>
-                    <p>{cartoon.show}</p></div>
-
+                    />
+                    <h5 className="card-title">{cartoon.character}</h5>
+                    <p className="card-text">{cartoon.show}</p>
+                </div>
             </div>
         );
     };
 
     return (
-        <div className="container d-flex p-3 ">
-
+        <div className="container d-flex flex-wrap justify-content-center p-3">
             {mtvCartoons.map((cartoon) => (
                 <CartoonCard key={cartoon.id} cartoon={cartoon} />
             ))}
-
         </div>
-
-    )
+    );
 }
