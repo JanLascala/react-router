07cc9267dc1db161eb1268bf9936eb0d2ff789ac
JanLascala <lascala.jan@gmail.com>
2025-04-01 16:41:37 +0200

    fix: update Home page content and adjust Jumbo component styling

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,11 +5,11 @@ export default function Home() {
                 <div class="row justify-content-center">
                     <div class="col-md-6">
                         <div
-                            class="h-100 p-5 text-white border rounded-3 Jumbo"
+                            class="p-5 text-white border rounded-3 Jumbo"
                         >
                             <h2> Daria</h2>
                             <p>
-
+                                Daria was an animated TV show that aired on MTV from 1997 to 2002. A spin-off of Beavis and Butt-Head, it followed the sharp-witted, sarcastic Daria Morgendorffer as she navigated high school life in the fictional town of Lawndale. With its dry humor, social satire, and smart, cynical protagonist, Daria became a cult classic, offering a sharp critique of teenage life, pop culture, and suburban society. The show stood out for its intelligent writing, strong female lead, and themes of individuality and skepticism.
                             </p>
                             <button
                                 class="btn btn-warning"
