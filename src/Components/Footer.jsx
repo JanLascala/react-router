b8a3867d883d3e4d0ad260e489838f5158036751
JanLascala <lascala.jan@gmail.com>
2025-04-01 15:25:43 +0200

    initialize React application with Vite setup and basic components (forgot to commit)

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+    return (
+        <>
+            <div
+                class="row-cols-4 justify-content-center align-items-center g-2"
+            >
+                <div class="col">Column</div>
+                <div class="col">Column</div>
+                <div class="col">Column</div>
+            </div>
+
+        </>
+    )
+}
