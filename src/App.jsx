e6bc8236a5c742de9493b15ac7990e20376796a9
JanLascala <lascala.jan@gmail.com>
2025-04-01 15:47:27 +0200

    add routing and create Home, About, and Posts pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+//layout import
+import DefaultLayout from "./DefaultLayout/DefaultLayout";
+//import pages
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Posts from "./Pages/Posts";
 function App() {
 
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={DefaultLayout}>
+            <Route path="/" Component={Home} />
+            <Route path="/posts" Component={Posts} />
+            <Route path="/about" Component={About} />
 
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
