0aa0100ffd3e1e1a21c12e96289a25b3d536e180
JanLascala <lascala.jan@gmail.com>
2025-04-02 14:51:25 +0200

    refactor: remove unused CartoonCard component and clean up Posts page

--- src/Pages/Posts.jsx
+++ src/Pages/Posts.jsx
@@ -6,32 +6,11 @@ export default function Posts() {
     const navigate = useNavigate();
 
     const CartoonCard = ({ cartoon }) => {
-        return (
-            <div className="card bg-secondary text-light m-3" >
-                <div className="card-body text-center">
-                    <img
-                        className="card-img-top mb-3"
-                        src={cartoon.image}
+        return ()
 
-                    />
-                    <h5 className="card-title">{cartoon.character}</h5>
-                    <p className="card-text">{cartoon.show}</p>
-                    <button
-                        className="btn btn-primary mt-2"
-                        onClick={() => navigate(`/post/${cartoon.id}`)}
-                    >
-                        View Details
-                    </button>
-                </div>
-            </div>
-        );
+
     };
 
-    return (
-        <div className="container d-flex flex-wrap justify-content-center p-3">
-            {mtvCartoons.map((cartoon) => (
-                <CartoonCard key={cartoon.id} cartoon={cartoon} />
-            ))}
-        </div>
-    );
+
+
 }
