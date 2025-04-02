576cd472f996a2dc4f65bcad3302171e8b744efc
JanLascala <lascala.jan@gmail.com>
2025-04-02 16:31:28 +0200

    feat: enhance Post component with navigation and detailed character display

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,16 +1,39 @@
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import mtvCartoons from "../Data/Characters";
 
 export default function Post() {
     const { id } = useParams();
 
+    const cartoon = mtvCartoons.find(cartoon => cartoon.id === parseInt(id));
+
     useEffect(() => {
 
     }, []);
-    return (
-        <div>
-            <h1>character name:{id} </h1>
-        </div>
 
-    )
+    const navigate = useNavigate();
+    return (
+        <>
+            <div className="d-flex justify-content-spacebetween">
+                <button className="m-4"
+                    onClick={() => navigate(-1)}>
+                    Back
+                </button>
+                <button className="m-4"
+                    onClick={() => navigate(cartoon.id + 1)}>
+                    forward
+                </button></div>
+
+            <div className="d-flex justify-content-center g-3 mt-4">
+                <img className="portrait p-3" src={cartoon.image} alt={cartoon.character} />
+                <div>
+                    <h1>Character Name: {cartoon.character}</h1>
+                    <p>Show: {cartoon.show}</p>
+                    <h2>description</h2>
+                    <p>tba...</p>
+                </div>
+            </div>
+
+        </>
+    );
 }
